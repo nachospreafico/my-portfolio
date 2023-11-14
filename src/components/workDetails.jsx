e1317ef439a546581f18575ce0352c6f6df3edda
Ignacio Spreafico <nachospreafico06@gmail.com>
2023-11-14 10:38:45 +0100

    Added work experience

--- src/components/workDetails.jsx
+++ src/components/workDetails.jsx
@@ -6,7 +6,7 @@ export const workDetails = [
     location: "Barcelona, Spain",
     role: "Demand Planner",
     tasks:
-      "My day to day consists primarily of: data entry and analysis in the gPSI system, POS data recollection and upload into SAP BW, reports generation (using Excel and Power BI) and maintenance for Sales and Marketing teams, overseeing the Sales Forecast and upload it into our system, among other tasks.",
+      "My day to day consists primarily of: data entry and analysis in the gPSI system, POS data recollection and upload into SAP BW, reports generation (using Excel and Power BI) and maintenance for Sales and Marketing teams, overseeing the Sales Forecast and upload it into our system, automating tasks with python and VBA scripts, among other tasks.",
     skills:
       "Excel, SAP HANA, IBM Lotus Notes, Qliksense, Analysis for Office (AFO), PowerBI, Microsoft Outlook, Python and VBS automation scripts",
   },
@@ -18,25 +18,36 @@ export const workDetails = [
     role: "Administrative Assistant",
     tasks:
       "I was in charge of answering phone calls and emails, develop and maintain contact lists and provide support to clients.",
+    skills: "Excel, Office equipment",
   },
   {
     from: "Mar - 2018",
-    toDate: "Apr - 2020",
+    toDate: "Apr - 2021",
     company: "Sanatorio Juncal",
     location: "Buenos Aires, Argentina",
     role: "Administrative Assistant",
     tasks:
       "I was tasked with answering phone calls and emails, develop and maintain contact lists and provide support to clients, act as the point of contact for internal and external clients, order office supplies and research new deals and suppliers, among other tasks.",
-    skills: "Excel, Office equipment, Power Point, Word",
+    skills: "Excel, Office equipment, Power Point, Word, Billing System",
   },
   {
-    from: "Mar - 2018",
-    toDate: "Apr - 2020",
+    from: "Jul - 2015",
+    toDate: "Aug - 2017",
+    company: "Fundacion Cientifica del Sur",
+    location: "Buenos Aires, Argentina",
+    role: "Administrative Assistant",
+    tasks:
+      "I was tasked with answering phone calls and emails, develop and maintain contact lists and provide support to clients, act as the point of contact for internal and external clients, order office supplies and research new deals and suppliers, among other tasks.",
+    skills: "Excel, Office equipment, Power Point, Word,  Billing System",
+  },
+  {
+    from: "Jul - 2009",
+    toDate: "Mar - 2011",
     company: "Private Medical Practice",
     location: "Buenos Aires, Argentina",
     role: "Administrative Assistant",
     tasks:
       "I was responsible for patient reception, office maintainance, clinical history creation and maintainance, scheduling appointments, answering phone calls and emails and charging health insurances.",
-    skills: "Excel, Office equipment, Power Point, Word",
+    skills: "Excel, Office equipment",
   },
 ];
